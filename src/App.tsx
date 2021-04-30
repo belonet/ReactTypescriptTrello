a2d8084e03bd447cfc512240978b75d2e4b14677
belonet <p.belopotocan@gmail.com>
2021-04-30 14:02:10 +0200

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./styles.css";
+import { AppContainer } from "./styles";
+import { Column } from "./Column";
+import { Card } from "./Card";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typings" />
+      </Column>
+    </AppContainer>
   );
 }
